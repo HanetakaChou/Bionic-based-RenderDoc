4f7697c2c5e646bff613af70fa173941a58a0749
YuqiaoZhang <yuqiaozhang@aliyun.com>
2020-03-24 18:44:23 +0800

    update README.md

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 cd "$(dirname "$(readlink -f "${0}")")"/bionic-server/lib64
 export ENABLE_VULKAN_RENDERDOC_CAPTURE=1  
 export RENDERDOC_CAPOPTS=ababaaabaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaa #use /proc/**PID**/environ to view the options
 ./vksmoketest & 
+echo "use File -> Attach to Running Instance in qrenderdoc"
